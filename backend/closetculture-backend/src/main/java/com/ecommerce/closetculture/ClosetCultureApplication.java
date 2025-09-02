26c9b0316b9b01263ca7c0f96a263f0dcc9734e6
FaizhalRahuman <faizhalrahuman2004@gmail.com>
2025-09-02 17:21:25 +0530

    test code removed from ClosetCultureApplication.java

--- backend/closetculture-backend/src/main/java/com/ecommerce/closetculture/ClosetCultureApplication.java
+++ backend/closetculture-backend/src/main/java/com/ecommerce/closetculture/ClosetCultureApplication.java
@@ -1,8 +1,5 @@
 package com.ecommerce.closetculture;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -24,17 +21,6 @@ public class ClosetCultureApplication {
 
 		SpringApplication.run(ClosetCultureApplication.class, args);
 
-        try {
-            String url = "jdbc:mysql://nozomi.proxy.rlwy.net:56538/railway";
-            String user = "root";
-            String pass = "mOomWoxYDwrNJJElLCJcqnThbNSXKLsS";
-
-            Connection conn = DriverManager.getConnection(url, user, pass);
-            System.out.println("Connected successfully!");
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
